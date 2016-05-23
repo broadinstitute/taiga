cd03260f0703103ab1defdb7efd623acea5ee938
Philip Montgomery <pmontgom@broadinstitute.org>
2016-05-23 15:36:38 -0400

    dropped version specific requirement for h5py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=['taiga'],
     install_requires=["numpy", "scipy", "Flask==0.10.1", "Flask-Injector==0.3.1", "Flask-SQLAlchemy==1.0", "Jinja2==2.7.2", "SQLAlchemy==0.9.4", 
-        "alembic==0.6.4", "gunicorn==18.0", "h5py==2.2.1", "injector==0.8.0", "nose==1.3.0", "requests==2.2.1", "Flask-OpenID==1.2.1", "markdown==2.4.1"],
+        "alembic==0.6.4", "gunicorn==18.0", "h5py", "injector==0.8.0", "nose==1.3.0", "requests", "Flask-OpenID==1.2.1", "markdown==2.4.1"],
     author="Philip Montgomery",
     author_email="pmontgom@broadinstitute.org",
     long_description="A tool for sharing tabular and matrix datasets",
