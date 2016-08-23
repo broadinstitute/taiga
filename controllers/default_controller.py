c4083493d2c9f08baad1fd206c644a64d411ecfd
Philip Montgomery <pmontgom@broadinstitute.org>
2016-08-22 22:27:04 -0400

    fixed folder calls

--- controllers/default_controller.py
+++ controllers/default_controller.py
@@ -10,12 +10,12 @@ def get_folder(folderId):
     parents = [dict(name=f.name, id=f.id) for f in db.get_parent_folders(folderId)]
     entries = []
     for e in folder['entries']:
-        if e.type == "folder":
-            name = db.get_folder(e.id).name
-        elif e.type == "dataset":
-            name = db.get_dataset(e.id).name 
-        elif e.type == "datasetVersion":
-            name = db.get_dataset_version(e.id).name
+        if e['type'] == "folder":
+            name = db.get_folder(e['id'])['name']
+        elif e['type'] == "dataset":
+            name = db.get_dataset(e['id'])['name']
+        elif e['type'] == "datasetVersion":
+            name = db.get_dataset_version(e['id'])['name']
         else:
             raise Exception("Unknown entry type: {}".format(e.type))
 
@@ -37,11 +37,17 @@ def get_user():
     user = current_app.db.get_user(_get_user_id())
     return flask.jsonify(user) 
 
-def create_folder():
+def create_folder(metadata):
+    db = current_app.db
+
+    folder_id = db.add_folder(metadata['name'], 'folder', metadata['description'])
+    db.add_folder_entry(metadata['parent'], folder_id, 'folder')
+
+    return flask.jsonify(id=folder_id, name=metadata['name'])
+
+def update_folders(operations):
     pass
 
-def update_folders():
-    pass
 def get_upload_url():
     pass
 
