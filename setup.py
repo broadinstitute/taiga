ef271063a38b98af3cc5fa8a722776bf69270ced
Philip Montgomery <pmontgom@broadinstitute.org>
2016-11-28 10:04:18 -0500

    Added list of requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
+#required
+#attrs (16.2.0)
+#connexion (2016.0.dev1, /Users/pmontgom/dev/connexion)
+#Flask (0.11.1)
+#requests (2.11.0)
+#six (1.10.0)
+#tinydb (3.2.1)
+
 setup(
     name='taiga2',
     version='0.1',
