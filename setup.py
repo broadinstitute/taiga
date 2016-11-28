f06ae1153857137a641e2e7800ae4e4007e19014
Remi Marenco <rmarenco@broadinstitute.org>
2016-11-28 11:19:51 -0500

    Add the dependencies to setup.py and minor change to handle the version

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+import taiga2
+
 #required
 #attrs (16.2.0)
 #connexion (2016.0.dev1, /Users/pmontgom/dev/connexion)
@@ -10,10 +12,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='taiga2',
-    version='0.1',
-    packages=['taiga2'],
+    version=taiga2.__version__,
+    packages=find_packages(),
     author="Philip Montgomery",
     author_email="pmontgom@broadinstitute.org",
     entry_points={'console_scripts': [
-        "taiga2 = taiga2.app:main" ]}
+        "taiga2 = taiga2.app:main" ]},
+    install_requires=[
+        'attrs==16.2.0',
+        'connexion>=1.0.129',
+        'Flask==0.11.1',
+        'requests==2.11.0',
+        'six==1.10.0',
+        'tinydb==3.2.1',
+    ],
 )
