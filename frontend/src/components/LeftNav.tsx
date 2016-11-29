77edf76eec98791dc870f6559c63c917ceea0641
Remi Marenco <rmarenco@broadinstitute.org>
2016-11-29 10:47:31 -0500

    Replace element.id by the map index to help React reconciliation

--- frontend/src/components/LeftNav.tsx
+++ frontend/src/components/LeftNav.tsx
@@ -12,8 +12,8 @@ export interface LeftNavProps {
 
 export class LeftNav extends React.Component<LeftNavProps, {}> {
     render() {
-        let items = this.props.items.map(element => {
-            return <li key={element.id} onClick={ element.action }>{element.label}</li>;
+        let items = this.props.items.map((element, index) => {
+            return <li key={index} onClick={ element.action }>{element.label}</li>;
         });
 
         return <div id="left-nav"><ul>{ items }</ul></div>
