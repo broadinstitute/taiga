a457643740a78470aaa6c2bf9c0819b50b7ef244
Remi Marenco <rmarenco@broadinstitute.org>
2017-01-09 15:18:34 -0500

    Add DataFile and Activity

--- taiga2/models.py
+++ taiga2/models.py
@@ -2,10 +2,12 @@ import enum
 import uuid
 import re
 
-from sqlalchemy import Table, Column, Integer, String, Text, ForeignKey, Enum
+from sqlalchemy import Table, Column, Integer, String, Text, ForeignKey, Enum, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.orm import column_property
+from sqlalchemy import select, func
 
 Base = declarative_base()
 engine = create_engine('sqlite:///taiga2.db', echo=True)
@@ -18,6 +20,7 @@ session = Session()
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 # STOP delete
+
 
 def generate_permaname(name):
     """Generate a permaname based on uuid and the original name"""
@@ -66,8 +69,6 @@ class Entry(Base):
     name = Column(String(80), unique=True)
     type = Column(String(50))
 
-    print(classmethod.__class__.__name__,)
-
     __mapper_args__ = {
         'polymorphic_identity': classmethod.__class__.__name__,
         'polymorphic_on': type,
@@ -99,17 +100,11 @@ class Folder(Entry):
     creator_id = Column(Integer, ForeignKey("users.id"))
 
     creator = relationship("User",
-                           backref=__tablename__)
+                           backref="user")
 
     __mapper_args__ = {
         'polymorphic_identity': "Folder"
     }
-
-
-class Datafile(Base):
-    __tablename__ = 'datafiles'
-
-    id = Column(Integer, primary_key=True)
 
 
 class Dataset(Entry):
@@ -119,9 +114,6 @@ class Dataset(Entry):
 
     description = Column(Text, default="No description provided")
 
-    versions = relationship("DatasetVersion",
-                            backref="dataset")
-
     # TODO: Use the name/key of the dataset and add behind the uuid?
     permaname = Column(Text, unique=True, nullable=False)
 
@@ -129,17 +121,82 @@ class Dataset(Entry):
         'polymorphic_identity': "Dataset"
     }
 
+# Association for Many to Many relationship between dataset_version and datafile
+datasetVersion_dataFile_association_table = Table('datasetVersion_dataFile_association',
+                                                  Base.metadata,
+                                                  Column('datasetversion_id', Integer, ForeignKey('dataset_versions.id')),
+                                                  Column('datafile_id', Integer, ForeignKey('datafiles.id'))
+                                                  )
+
 
 class DatasetVersion(Entry):
     __tablename__ = 'dataset_versions'
 
-    id = Column(Integer, ForeignKey('entries.id'), primary_key=True)
+    id = Column(Integer,
+                ForeignKey('entries.id'),
+                primary_key=True)
+
+    creator_id = Column(Integer, ForeignKey("users.id"))
+
+    creator = relationship("User",
+                           backref=__tablename__)
+
+    dataset_id = Column(Integer, ForeignKey("datasets.id"))
+
+    dataset = relationship("Dataset",
+                           foreign_keys=[dataset_id],
+                           backref=__tablename__)
+
+    # Filled out by the server
+    version = Column(Integer)
+
+    datafiles = relationship("DataFile",
+                             secondary=datasetVersion_dataFile_association_table,
+                             backref=__tablename__)
 
     __mapper_args__ = {
         'polymorphic_identity': "DatasetVersion"
     }
 
 
-class Activity(Base):
-    __tablename__ = 'activities'
+class DataFile(Base):
+    # TODO: Can we create a datafile without including it in a datasetVersion?
+    __tablename__ = 'datafiles'
+
     id = Column(Integer, primary_key=True)
+
+    name = Column(String(80))
+
+    # To be able to differentiate multiple files with the same name
+    permaname = Column(Text, unique=True, nullable=False)
+
+    type = Column(String(80))
+
+    url = Column(Text, unique=True)
+
+
+class Activity(Base):
+    class ActivityType(enum.Enum):
+        created = "Created"
+        changed_name = "Changed name"
+        changed_description = "Changed Description"
+        added_version = "Added version"
+
+    __tablename__ = 'activities'
+
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User",
+                        backref=__tablename__)
+
+    dataset_id = Column(Integer, ForeignKey("datasets.id"))
+
+    dataset = relationship("Dataset",
+                           backref=__tablename__)
+
+    # We would want the type of change and the comments associated
+    type = Column(Enum(ActivityType))
+
+    comments = Column(Text)
