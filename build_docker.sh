590a30598d9b61dcc16a0e8b1c0c40eaaf8e2c9b
Remi Marenco <rmarenco@broadinstitute.org>
2017-02-09 18:17:58 -0500

    Fix the change of folder impact on the script + fix rights of the travis aws user

--- build_docker.sh
+++ build_docker.sh
@@ -3,4 +3,4 @@ set -ev
 
 # TODO: Need to see how to tar from travis/
 tar --exclude='dist' --exclude='*/taiga.tar.gz' --exclude='.idea' --exclude='node_modules' --exclude='frontend/node_modules' --exclude='.git' -zcvf docker/taiga.tar.gz *
-docker build -t taiga:latest ../docker/
+docker build -t taiga:latest docker/
