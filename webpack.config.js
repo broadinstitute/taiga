4d3a0640da0d82696a3881825cd960da6d438bcc
Remi Marenco <rmarenco@broadinstitute.org>
2017-02-10 18:02:26 -0500

    Change webpack version and modify the way it handles aws and pre-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,22 +15,20 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
 
     module: {
-        loaders: [
+        rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            { test: /\.json$/, loaders: ['json'] }
-        ],
-
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.json$/, loaders: ['json-loader'] },
             {
+                enforce: 'pre',
                 test: /\.js$/,
                 exclude: ["/node_modules/", "/frontend/node_modules/"],
-                loader: "source-map-loader" }
+                loader: "source-map-loader"
+            }
         ],
     },
 
