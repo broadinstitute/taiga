a2b7d01302a7b001f3067eba61b9e98b30853409
Remi Marenco <rmarenco@broadinstitute.org>
2017-02-16 09:38:54 -0500

    Add webpack production build in Dockerfile (for Travis)

--- build_docker.sh
+++ build_docker.sh
@@ -4,3 +4,6 @@ set -ev
 # TODO: Need to see how to tar from travis/
 tar --exclude='dist' --exclude='*/taiga.tar.gz' --exclude='.idea' --exclude='node_modules' --exclude='frontend/node_modules' --exclude='.git' -zcvf docker/taiga.tar.gz *
 docker build -t taiga:latest docker/
+
+# TODO: We are missing multiple steps:
+# - Retrieve the settings.cfg or create it in the VM?
