45e0f128d0b54080623a0e02468a655f08c9b1f8
Remi Marenco <rmarenco@broadinstitute.org>
2017-05-19 13:43:39 -0400

    Add dataset_version/uuid_ds

--- frontend/src/Index.tsx
+++ frontend/src/Index.tsx
@@ -88,7 +88,7 @@ class App extends React.Component<AppProps, any> {
                         Broad Institute, Cancer Program Data Science {(new Date()).getFullYear()}
                     </div>
                     <div className="login-box pull-right bottom-page-text">
-                        Rev 2.7.4
+                        Rev 2.8.0
                     </div>
                 </footer>
             </div>
@@ -214,6 +214,7 @@ tapi.get_user().then((user: User) => {
                 <IndexRoute component={Home}/>
                 <Route path="dataset/:datasetId" component={DatasetView as any}/>
                 <Route path="dataset/:datasetId/:datasetVersionId" component={DatasetView as any}/>
+                <Route path="dataset_version/:datasetVersionId" component={DatasetView as any}/>
                 <Route path="folder/:folderId" component={FolderView as any}/>
                 <Route path="token/" component={ Token as any }/>
                 <Route path="recentlyViewed/" component={ RecentlyViewed as any }/>
