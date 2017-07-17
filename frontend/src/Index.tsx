e03198855dc2e2318bdca1ee6d4520d9b44fd9b8
Remi Marenco <rmarenco@broadinstitute.org>
2017-07-17 16:38:52 -0400

    Add margin

--- frontend/src/Index.tsx
+++ frontend/src/Index.tsx
@@ -88,7 +88,7 @@ class App extends React.Component<AppProps, any> {
                         Broad Institute, Cancer Program Data Science {(new Date()).getFullYear()}
                     </div>
                     <div className="login-box pull-right bottom-page-text">
-                        Rev 2.9.3
+                        Rev 2.9.4
                     </div>
                 </footer>
             </div>
