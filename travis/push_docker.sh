6d43f717337e0edf31120e115daa5d848699c303
Remi Marenco <rmarenco@broadinstitute.org>
2017-12-15 16:56:48 -0500

    Change the right shell program for travis/docker

--- travis/push_docker.sh
+++ travis/push_docker.sh
@@ -3,7 +3,7 @@ set -ev
 
 # This script needs AWS environment to be set
 # REPO_URI needs also to be set as the repository URI
-docker_login=`aws ecr get-login --region us-east-1`
+docker_login=`aws ecr get-login --region us-east-1 --no-include-email`
 ${docker_login}
 docker tag taiga:latest ${REPO_URI}
 docker push ${REPO_URI}
