70c95303bb6a732ebbb67ead37aa3f5c1df40080
Philip Montgomery <pmontgom@broadinstitute.org>
2019-07-05 14:17:11 -0400

    bumped version of python in docker file. Also fixed build_docker.sh to fail if docker build fails.

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ev
+set -evx
 
 # TODO: Need to see how to tar from travis/
 tar --exclude='dist' --exclude='*/taiga.tar.gz' --exclude='.idea' --exclude='node_modules' --exclude='frontend/node_modules' --exclude='.git' -zcvf docker/taiga.tar.gz *
