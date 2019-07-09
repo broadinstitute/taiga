df724be9d9b0c8b2e3470754697f411b7f6f7193
Philip Montgomery <pmontgom@broadinstitute.org>
2019-07-09 13:08:40 -0400

    moved docker push into script section so that failure to push will fail the build. Also fixed typo in repo_uri

--- travis/push_docker.sh
+++ travis/push_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -exv
 
-REPO_URL=us.gcr.io/cds-docker-containers/taiga
+REPO_URI=us.gcr.io/cds-docker-containers/taiga
 
 cat travis/travis-docker-push-account.json | docker login -u _json_key --password-stdin https://us.gcr.io
 docker tag taiga:latest ${REPO_URI}
