5e1a4c8634667791eb33d65593fe7a49434ec83a
Yejia Chen <yejia@broadinstitute.org>
2019-07-16 10:32:09 -0400

    Add test for search

--- taiga2/tests/test_endpoint.py
+++ taiga2/tests/test_endpoint.py
@@ -859,6 +859,18 @@ def test_create_virtual_dataset_endpoint(session: SessionBase):
     assert entry.underlying_file_id != data_file_id_1
 
 
+def test_search_within_folder(session: SessionBase, new_dataset_in_new_folder_in_home):
+    current_user = models_controller.get_current_session_user()
+    home_folder_id = current_user.home_folder_id
+    search_query = "New Dataset in a folder"
+    r = get_data_from_flask_jsonify(
+        endpoint.search_within_folder(home_folder_id, search_query)
+    )
+    assert r["current_folder"]["id"] == home_folder_id
+    assert len(r["entries"]) == 1
+    assert r["entries"][0]["entry"]["id"] == new_dataset_in_new_folder_in_home.id
+
+
 # <editor-fold desc="User">
 # </editor-fold>
 
