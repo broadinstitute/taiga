9a66bc055ddbe39248962b9bd98677b650cc4f04
Philip Montgomery <pmontgom@broadinstitute.org>
2020-04-07 14:32:24 -0400

    updated gpg key for installing from cran

--- docker-base/deploy.sh
+++ docker-base/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 
-docker build . -t us.gcr.io/cds-docker-containers/taiga-base:v1
-docker push us.gcr.io/cds-docker-containers/taiga-base:v1
+docker build . -t us.gcr.io/cds-docker-containers/taiga-base:v2 && \
+docker push us.gcr.io/cds-docker-containers/taiga-base:v2
