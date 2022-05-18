1d9e3280089b079e079bbad1fa99138e486c827c
Ali Mourey <90707472+alimourey@users.noreply.github.com>
2022-05-18 10:24:58 -0400

    Took out temporary code added during testing

--- taiga2/auth.py
+++ taiga2/auth.py
@@ -95,11 +95,7 @@ def set_current_user_from_bearer_token():
     bearer_token = request.headers.get("Authorization", None)
     default_user_email = config.get("DEFAULT_USER_EMAIL", None)
 
-    if (
-        user is None
-        and bearer_token is not None
-        and request.headers.get("Host") != "127.0.0.1:5000"
-    ):
+    if user is None and bearer_token is not None:
         m = re.match("Bearer (\\S+)", bearer_token)
         if m is not None:
             token = m.group(1)
