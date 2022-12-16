85ee6f9c696e935fc0e8d9ae59d5e432091dee34
Philip Montgomery <pmontgom@broadinstitute.org>
2022-12-16 16:06:49 -0500

    added missing file changes

--- build_docker.sh
+++ build_docker.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -evx
 
+IMAGE_TAG="$1"
+
 # TODO: Need to see how to tar from travis/
 #tar --exclude='dist' \
 #--exclude='*/taiga.tar.gz' \
 #--exclude='*.pyc' \
 #--exclude='*~' \
 #--exclude='.idea' --exclude='node_modules' --exclude='frontend/node_modules' --exclude='.git' -zcvf docker/taiga.tar.gz *
-docker build -t taiga:latest .
+docker build -t ${IMAGE_TAG} .
 
 # TODO: We are missing multiple steps:
 # - Retrieve the settings.cfg or create it in the VM?
