a0b389afee7de64ac11ed4cd1766c37fe35ea68a
Philip Montgomery <pmontgom@broadinstitute.org>
2026-01-21 15:35:44 -0500

    moved python down to py3.10

--- docker/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# IMPORTANT: This script should not be used anymore. Now use ansible-config
-set -ev
-docker_image=784167841278.dkr.ecr.us-east-1.amazonaws.com/taiga:latest
-docker_login=`aws ecr get-login --region us-east-1 --no-include-email`
-${docker_login}
-#docker tag taiga:latest ${docker_image}
-#docker push ${docker_image}
-ssh ubuntu@cds.team  "${docker_login} && docker pull ${docker_image} && sudo systemctl restart taiga"
