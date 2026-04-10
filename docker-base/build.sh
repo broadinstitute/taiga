bda61ea0fc6fc0c9c20bce873ef8264f0fbb0884
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2026-04-10 10:01:10 -0400

    Removed some deployment related dead code and updated documentation

--- docker-base/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xe
-
-docker build . -t us.gcr.io/cds-docker-containers/taiga-base:v2 && \
-docker push us.gcr.io/cds-docker-containers/taiga-base:v2
--- docker/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -ev
-tar --exclude='dist' --exclude='*/taiga.tar.gz' --exclude='.idea' --exclude='node_modules' --exclude='frontend/node_modules' --exclude='.git' -zcvf docker/taiga.tar.gz *
-docker build -t taiga:latest docker/
